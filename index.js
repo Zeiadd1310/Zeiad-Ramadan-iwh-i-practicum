3880a4046c18fd125e289934027db7164a42b2f4
Zeiad Ramadan <zeiadramadan6@gmail.com>
2025-12-02 17:41:02 +0200

    Update README and other changes

--- index.js
+++ index.js
@@ -1,71 +1,65 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// HubSpot API token
+const HUBSPOT_TOKEN = process.env.HUBSPOT_TOKEN;
+const HUBSPOT_API = 'https://api.hubapi.com/crm/v3/objects/contacts';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// -------------------- ROUTES -------------------- //
 
-// * Code for Route 1 goes here
+// Homepage: list all contacts (pets)
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(`${HUBSPOT_API}?properties=name_pet,pet_age,pet_gender,pet_type`, {
+      headers: { Authorization: `Bearer ${HUBSPOT_TOKEN}` }
+    });
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+    const contacts = response.data.results;
+    res.render('homepage', { contacts });
+  } catch (error) {
+    console.error(error);
+    res.send('Error fetching contacts');
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// GET form to create new contact (pet)
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Update Contact (Pet) Form | Integrating With HubSpot I Practicum' });
 });
-*/
 
+// POST: create new contact (pet)
+app.post('/update-cobj', async (req, res) => {
+  try {
+    const { name_pet, pet_age, pet_gender, pet_type } = req.body;
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+    await axios.post(HUBSPOT_API, {
+      properties: {
+        name_pet,
+        pet_age,
+        pet_gender,
+        pet_type
+      }
+    }, {
+      headers: { Authorization: `Bearer ${HUBSPOT_TOKEN}` }
+    });
+
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.send('Error creating contact');
+  }
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
